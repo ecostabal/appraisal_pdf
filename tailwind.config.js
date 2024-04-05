55e46db62d233fcf2dc5462400122e0a2d73fdb4
Pucho Dev <111137039+ecostabal@users.noreply.github.com>
2024-04-05 03:31:24 -0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
